50aef3b1879c8b54cf5165b6240ec34d6aabe855
yiming fan <yimingfan@yimingdeMacBook-Pro.local>
2018-11-20 12:10:29 +1100

    add cros in app.js

--- app.js
+++ app.js
@@ -14,6 +14,16 @@ var databaseRouter = require('./routes/database');
 var app = express();
 
 
+//设置跨域访问，在其他设置前先设置这个
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
